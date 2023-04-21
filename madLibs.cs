9ec4adf86dec351b7246c80d2782a9fe9e0f0d09
ntanimate <87493946+ntanimate@users.noreply.github.com>
2023-04-21 15:52:30 -0700

    Update madLibs.cs

--- madLibs.cs
+++ madLibs.cs
@@ -7,8 +7,8 @@ namespace MadLibs
     static void Main(string[] args)
     {
       /*
-      Madlib program 
-      Author: Ninette T
+      Madlib program ...
+      Ninette: ...
       */
 
 
@@ -26,33 +26,36 @@ namespace MadLibs
       string adjective1 = Console.ReadLine();
       Console.WriteLine("Type in another adjective.");
       string adjective2 = Console.ReadLine();
+      Console.WriteLine("Type in another adjective.");
+      string adjective3 = Console.ReadLine();
+      Console.WriteLine("Type in an animal.");
+      string animal = Console.ReadLine();
+      Console.WriteLine("Type in a food.");
+      string food = Console.ReadLine();
       Console.WriteLine("Type in a noun.");
-      string noun = Console.ReadLine();
+      string noun1 = Console.ReadLine();
       Console.WriteLine("Type in another noun.");
       string noun2 = Console.ReadLine();
-      Console.WriteLine("Type in another noun.");
-      string noun3 = Console.ReadLine();
-      Console.WriteLine("Type in a present tense verb.");
+      Console.WriteLine("Type in a verb.");
       string verb = Console.ReadLine();
-      Console.WriteLine("Type in a type of music.");
-      string musicType = Console.ReadLine();
-      Console.WriteLine("Type in an emotion.");
-      string emotion = Console.ReadLine();
-      Console.WriteLine("Type in an adverb.");
-      string adverb = Console.ReadLine();
-      Console.WriteLine("Type in a name of a place.");
-      string place = Console.ReadLine();
-      Console.WriteLine("Type in a different name of a person.");
-      string name2 = Console.ReadLine();
+      Console.WriteLine("Type in a type of fruit.");
+      string fruit = Console.ReadLine();
+      Console.WriteLine("Type in an dessert.");
+      string dessert = Console.ReadLine();
+      Console.WriteLine("Type in a superhero.");
+      string superhero = Console.ReadLine();
+      Console.WriteLine("Type in a name of a country.");
+      string country = Console.ReadLine();
       Console.WriteLine("Type in a year.");
       string year = Console.ReadLine();
       
-      // The template for the story, print out:
+      // The template for the story:
 
-      Console.WriteLine($"This morning {name} woke up feeling {adjective1}. 'It is going to be a {adjective2} day!' Outside, a bunch of {noun}s were protesting to keep {noun2} in stores. They began to {verb} to the rhythm of the {musicType}, which made all the {noun2}s very {emotion}. Concerned, {name} texted {name2}, who flew {adverb} to {place} and dropped {noun} in a puddle of frozen {noun3}. {name} woke up in the year {year}, in a world where {noun2}s ruled the world.");
+      string story = $"This morning {name} woke up feeling {adjective1}. 'It is going to be a {adjective2} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {noun1}, which made all the {fruit}s very {adjective3}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {noun2}s ruled the world.";
 
 
-     
+      // Print the story:
+      Console.WriteLine(story);
       
     }
   }
